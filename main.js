938f695ab42f672f398e39634a5f85e951d5c8c4
shukurenai09 <78141688+jashpanchal@users.noreply.github.com>
2022-05-26 12:17:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+objects = [];
 status = "";
-
+function preload(){
+    var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+}
 function setup(){
     canvas = createCanvas(340,250);
     canvas.center();
@@ -16,9 +21,45 @@ function start(){
 
 function modelLoaded(){
     console.log("modelLoaded");
-    status = true;
+    status = true;  
 }
 
 function draw(){
-    image(video,0,0,340,250)
+    image(video,0,0,340,250);
+    if(status != ""){
+        objectDetector.detect(video,gotResults);
+    for(i=0;i<objects.length;i++){
+        percent = floor(objects[i].confidence * 100);
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        fill("#FF0000");
+        text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+        noFill();
+        stroke("#0000FF");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        if(objects[i].label == input_value){
+            video.stop();
+            objectDetector.detect(gotResults);
+            document.getElementById("object_found").innerHTML = "Object Mentioned Found";
+            speak();
+        }
+        else{
+            document.getElementById("object_found").innerHTML = "Object Mentioned Not Found";
+        }
+    }
+    }
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }    
+    objects = results;
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+            speak_data = "object mention found!";
+            var utterThis = new SpeechSynthesisUtterance(speak_data);
+        
+            synth.speak(utterThis);
 }
